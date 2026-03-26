170058bba24a7989ed5aec2378fb5460ab54030e
yashraj-rgb <mauryayashraj833@gmail.com>
2026-03-26 10:49:26 +0530

    Add files via upload

---
+++ gtu papers fianl site/data/sem2.js
@@ -0,0 +1,128 @@
+console.log("sem2.js loaded ✅");
+
+window.SEM2_DATA = {
+  maths2: {
+    name: "Engineering Mathematics – II",
+    winter: {
+      2021: "https://drive.google.com/uc?export=download&id=1cpD7NLbCmgo0s1tyyQGEq6PDeuDobAKK",
+      2019: "https://drive.google.com/uc?export=download&id=1qAARnMPPOzmZFRpyzSCXfZQV90n8fM_2",
+      2022: "https://drive.google.com/uc?export=download&id=11oVNtBbizo9VOtH3MoWGgC4BHm4M0rnN",
+      2018: "https://drive.google.com/uc?export=download&id=1kSVAa9mmiDfiYz0Jd-27LlDWdA-bpvUL",
+      2024: "https://drive.google.com/uc?export=download&id=11_7299Nx0wgNEwSK_Cdkldhmcv1M23-X",
+      2025: "https://drive.google.com/uc?export=download&id=1_oacnGkwlDq_9YcJQqZ_Z6Tws8p_mlY9"
+    },
+    summer: {
+      // 2020: "https://drive.google.com/uc?export=download&id=1qAARnMPPOzmZFRpyzSCXfZQV90n8fM_2",
+      2019: "https://drive.google.com/uc?export=download&id=1JnrNUWgBjZAUMn8aii1maqVwVIixF361",
+      2022: "https://drive.google.com/uc?export=download&id=19xPVNIfKgG7C-OxGqo9YlVxBe73hdx7h",
+      2023: "https://drive.google.com/uc?export=download&id=1QJbdn6SzC4YJl_TWOV_xUCQjg_CRuqa0",
+      2025: "https://drive.google.com/uc?export=download&id=1pSlsqqjRcqt4Mq-_JbSRAKk_5wQ1mORM",
+      2024: "https://drive.google.com/uc?export=download&id=1TWTrhDXEUJ_pxptzZbpqt-dTJw6UcPwt"
+    }
+  },
+
+  bee: {
+    name: "Basic Electrical Engineering",
+    winter: {
+      2025: "https://gtu.ac.in/uploads/W2025/BE/BE01R00051.pdf",
+      2024: "https://drive.google.com/uc?export=download&id=1BEEMEjHThQeW7ZXDU05fVWQQWhNIcHld",
+      2022: "https://drive.google.com/uc?export=download&id=1xhb3Fm37TFQC4GxcFGyhC4m6x3ZAoLYq",
+      2018: "https://drive.google.com/uc?export=download&id=1K_3cpSP10OeC__F-SZyv5Crek5t9VCxV",
+      2021: "https://drive.google.com/uc?export=download&id=1_uRj_clKF0Xc5j9KbU78vI99Cs7Q5rfm",
+      2019: "https://drive.google.com/uc?export=download&id=1a88cLL6NJl1XffixiTmhuysOAa-v9fsw"
+    },
+    summer: {
+      2025: "https://gtu.ac.in/uploads/S2025/BE/BE01000051.pdf",
+      2023: "https://drive.google.com/uc?export=download&id=1d0GMH19BMbwl8euCyiJmqLBgURd3F7PJ",
+      2022: "https://drive.google.com/uc?export=download&id=1HsLoIJTiv7FA9wOxoot28fuWKHXgMFLd",
+      2020: "https://drive.google.com/uc?export=download&id=1kToz6XU_kR5UI9LExiVK4cOadudUqnaZ",
+      2019: "https://drive.google.com/uc?export=download&id=1kApgLKogEUyiFCjgjHCOw_bdftyNfp6F",
+      2025: "https://gtupaper.com/wp-content/uploads/2026/01/BEE-S2025-3110005-GTUPaper.com_.pdf",
+      2024: "https://gtupaper.com/wp-content/uploads/2026/01/BEE-S2024-3110005-GTUPaper.com_.pdf"
+    }
+  },
+
+  bme: {
+    name: "Basic Mechanical Engineering",
+    winter: { 
+      2025: "https://gtu.ac.in/uploads/W2025/BE/3110006.pdf",
+      2024: "https://gtupaper.com/wp-content/uploads/2026/01/BME-W2024-3110006-GTUPaper.com_.pdf",
+      2023: "https://gtupaper.com/wp-content/uploads/2026/01/BME-W2023-3110006-GTUPaper.com_.pdf",
+      2022: "https://gtupaper.com/wp-content/uploads/2026/01/BME-W2022-3110006-GTUPaper.com_.pdf",
+       2021: "https://gtu.ac.in/uploads/W2021/BE/3110006.pdf",
+      2020: "https://gtu.ac.in/uploads/W2020/BE/3110006.pdf",
+      
+    },
+    summer: {
+       2025: "https://gtupaper.com/wp-content/uploads/2026/01/BME-S2025-3110006-GTUPaper.com_.pdf",
+      2024: "https://gtupaper.com/wp-content/uploads/2026/01/BME-S2024-3110006-GTUPaper.com_.pdf",  
+      2023: "https://gtupaper.com/wp-content/uploads/2026/01/BME-S2023-3110006-GTUPaper.com_.pdf",
+      2022: "https://gtupaper.com/wp-content/uploads/2026/01/BME-S2022-3110006-GTUPaper.com_.pdf",
+      2020: "https://gtu.ac.in/uploads/S2020/BE/3110006.pdf",
+    }
+  },
+  phy: {
+    name: "Engineering Physics – II",
+    winter: {
+      2025: "https://gtu.ac.in/uploads/W2025/BE/BE01R00021.pdf",
+      2024: "https://gtu.ac.in/uploads/W2024/BE/BE01000021.pdf",
+      2023: "https://gtupaper.com/wp-content/uploads/2026/01/Physics-W2023-2110011-GTUPaper.com_.pdf",
+      2022: "https://gtupaper.com/wp-content/uploads/2026/01/Physics-W2022-2110011-GTUPaper.com_.pdf",
+    },
+    summer: {
+        2025: "https://gtu.ac.in/uploads/S2025/BE/BE01000021.pdf",
+      2023: "https://gtupaper.com/wp-content/uploads/2026/01/Physics-S2023-2110011-GTUPaper.com_.pdf",
+      2024: "https://gtupaper.com/wp-content/uploads/2026/01/Physics-S2024-2110011-GTUPaper.com_.pdf"
+    }
+  },
+  eng: {
+    name: "English",
+    winter: {
+      2024: "https://gtupaper.com/wp-content/uploads/2026/01/English-W2024-3110002-GTUPaper.com_.pdf",
+      2023: "https://gtupaper.com/wp-content/uploads/2026/01/English-W2023-3110002-GTUPaper.com_.pdf",
+      2022: "https://gtupaper.com/wp-content/uploads/2026/01/English-W2022-3110002-GTUPaper.com_.pdf"
+    },
+    summer: {
+      2025: "https://gtupaper.com/wp-content/uploads/2026/01/English-S2025-3110002-GTUPaper.com_.pdf",
+      2024: "https://gtupaper.com/wp-content/uploads/2026/01/English-S2024-3110002-GTUPaper.com_.pdf",
+      2023: "https://gtupaper.com/wp-content/uploads/2026/01/English-S2023-3110002-GTUPaper.com_.pdf",
+      2022: "https://gtupaper.com/wp-content/uploads/2026/01/English-S2022-3110002-GTUPaper.com_.pdf"
+    }
+  },
+  fai: {
+    name: "Fundamentals of Artificial Intelligence",
+     winter: {
+     2025: "https://gtu.ac.in/uploads/W2025/BE/BE02000041.pdf",
+      2024: "https://drive.google.com/file/d/1_WxfhFWoQYuJx5mkhIFXzzp4N67AMCff/view?usp=sharing",
+     2023: "https://drive.google.com/file/d/1DuJtQyGOR2gsHH3CzRKpsBEBpEFUEMXC/view?usp=sharing",
+     2022: "https://drive.google.com/file/d/1ys2VP2_ExiAYGGI3u-vE1qaHlhGLpmVh/view?usp=sharing"
+     },
+    summer: {
+      2025: "https://gtu.ac.in/uploads/S2025/BE/BE02000041.pdf",
+      2024: "https://drive.google.com/file/d/1e_FbXaqYfYTunR6CGHHfTRILKjw84TpM/view?usp=sharing",
+      2023: "https://drive.google.com/file/d/1lli2-DLOPZlVJrfdCtBhHcwp8WVNgNHd/view?usp=sharing",
+    }
+  }
+,
+  syl: {
+    name: "Syllabus - Sem II",
+    winter: {
+      Maths: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/BE02R00011.pdf",
+      BEE: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/BE01R00051.pdf",
+      BME: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/BE01R00081.pdf",
+      Physics:"https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/BE01R00021.pdf",
+      English: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/BE02R00021.pdf",
+      FAI: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/BE02R00041.pdf",
+      Design_thinking: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/BE01R00071.pdf"
+    },
+    summer: {
+            Maths: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/BE02R00011.pdf",
+      BEE: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/BE01R00051.pdf",
+      BME: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/BE01R00081.pdf",
+      Physics:"https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/BE01R00021.pdf",
+      English: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/BE02R00021.pdf",
+      FAI: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/BE02R00041.pdf",
+      Design_thinking: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/BE01R00071.pdf"
+    }
+  }
+};
